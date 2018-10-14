d2244e57fa4a975b6d306d47e8959abfc1631f3e
midhun manohar <midhmanohar@gmail.com>
2018-10-14 22:24:51 +0530

    pset6

---
+++ pset6/caesar/caesar.py
@@ -0,0 +1,43 @@
+import sys
+from cs50 import get_string
+
+if(len(sys.argv)!=2):
+     print("Usage: ./caesar k")
+     exit(1)
+k=sys.argv[1]
+
+// TODO
+
+for i in k:
+    if(i.isalpha()):
+        continue
+    else:
+        print("Usage: ./vigenere k")
+        exit(1)
+
+l=len(k)
+p=get_string("plaintext: ")
+print("ciphertext:",end=" ")
+
+j=0
+key=0
+for i in p:
+    if(i.isalpha()):
+        if(k[j%l].isalpha()):
+            if(k[j%l].isupper()):
+                key=chr(ord(k[j%l])-ord('A'))
+            if(k[j%l].islower()):
+                key=chr(ord(k[j%l])-ord('a'))
+        else:
+            print(i,end="")
+
+        if(i.isupper()):
+            print("{}".format(chr((((ord(i)-ord('A'))+ord(key))%26)+ord('A'))),end="")
+            j=j+1
+        if(i.islower()):
+            print("{}".format(chr((((ord(i)-ord('a'))+ord(key))%26)+ord('a'))),end="")
+            j=j+1
+    else:
+        print("{}".format(i),end="")
+
+print()
